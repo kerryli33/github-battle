02a7e811fea49f397904dad4cccc4be9116cc545
kerryli33 <kerry2@ualberta.ca>
2020-08-02 15:42:19 -0600

    feature: highlight selected language

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,30 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            selectedLanguage: 'All'
+        }
+        this.changeLanguage = this.changeLanguage.bind(this)
+    }
+
+    changeLanguage(selectedLanguage) {
+        this.setState({
+            selectedLanguage
+        })
+    }
     render() {
         const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
         return (
             <ul className='flex-center'>
                 {languages.map( (language) => ( 
                     <li key={language}>
-                        <button className="btn-clear nav-link">
+                        <button 
+                            className="btn-clear nav-link"
+                            style={language === this.state.selectedLanguage ? {color: "#63ab7b"}
+                                                                            : null}
+                            onClick= {() => this.changeLanguage(language)}>
                             {language}
                         </button>
                     </li>
