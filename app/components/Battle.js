4625921ed3826c59d225db74cceca8f9191e81e9
kerryli33 <kerry2@ualberta.ca>
2020-08-15 23:57:17 -0600

    feature: added reset button

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -155,7 +155,14 @@ export default class Battle extends React.Component {
         const { playerOne, playerTwo } = this.state
 
         if (this.state.battle === true) {
-            return <Results playerOne={playerOne} playerTwo={playerTwo} />
+            return <Results 
+                     playerOne={playerOne} 
+                     playerTwo={playerTwo}
+                     onReset={() => this.setState({
+                         playerOne: null,
+                         playerTwo: null,
+                         battle: false
+                     })} />
         }
         return (
             <React.Fragment>
